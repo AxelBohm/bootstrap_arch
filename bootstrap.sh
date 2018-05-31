4cda539e0e7b04e40084d5528c79d7e6737712f4
AxelBohm <axel.boehm@ymail.com>
2018-05-31 20:04:40 +0200

    python

--- bootstrap.sh
+++ bootstrap.sh
@@ -58,3 +58,18 @@ for dotfile in .dotfiles/*; do
 done
 
 
+# python
+echo "python setup..."
+## miniconda
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
+bash ~/miniconda.sh -b -p ~/miniconda
+rm ~/miniconda.sh
+
+## modules
+echo "installing python modules..."
+conda install pip
+pip install pytest
+pip install pytest-watch
+pip install numpy
+pip install pandas
+
