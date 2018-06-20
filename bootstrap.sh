d2430b0eb82483d3ab2a479cd591e9eff011e061
AxelBohm <axel.boehm@ymail.com>
2018-06-20 23:49:07 +0200

    deleted unnecessary stuff because it was moved to dotfiles.

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,24 +13,9 @@ https://github.com/AxelBohm/bootstrap_arch.git
 ## usually zsh is already installed?
 chsh -s $(which zsh) # requires a restart to take action
 
-# oh-my-zsh
-# (check here what happens to the .zshr??? probably just do "rm .zshrc" afterwards?)
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-
-## syntax highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-
-## auto-suggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-
 # i3 wm
 echo "installing i3 wm..."
 sudo pacman -S i3-gaps
-
-
-## cloning config file
-git clone https://github.com/AxelBohm/i3-config.git .config/i3""
 
 
 # dropbox (headless install from dropbox website, build from AUR instead??)
@@ -40,9 +25,6 @@ wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf-
 
 # random stuff
 sudo pacman --noconfirm --needed -S calcurse htop cmatrix cowsay powerline-fonts
-
-# Vundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 
 # dotfiles
