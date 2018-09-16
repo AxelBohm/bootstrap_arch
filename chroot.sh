3c5e24f898b9b9bcbcdee37709f65c8964e04f65
AxelBohm <axel.boehm@ymail.com>
2018-09-16 22:34:15 +0200

    chroot script

---
+++ chroot.sh
@@ -0,0 +1,40 @@
+# Timezone stuff
+TZuser=$(cat timezone.tmp)
+ln -sf /usr/share/zoneinfo/$TZuser /etc/localtime
+hwclock --systohc
+
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+echo "en_US ISO-8859-1" >> /etc/locale.gen
+echo "de_DE.UTF-8 UTF-8" >> /etc/locale.gen
+echo "de_DE@euro ISO-8859-15" >> /etc/locale.gen
+locale-gen
+
+# set LANG variable
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+
+# pacman --noconfirm --needed -S networkmanager
+# systemctl enable NetworkManager
+# systemctl start NetworkManager
+
+pacman --noconfirm --needed -S grub && grub-install --target=i386-pc /dev/sda && grub-mkconfig -o /boot/grub/grub.cfg
+
+# set root password
+passwd
+
+###################
+# SETTING UP USER #
+###################
+read -r -p "Enter username:" username
+
+useradd -m -g wheel -s /bin/bash $name
+passwd $username
+
+# uncomment the line %wheel ALL=(ALL) ALL in sudoers file
+sed -i '/%wheel ALL=(ALL) ALL/s/^#//g' /etc/sudoers
+
+curl \
+  https://raw.githubusercontent.com/AxelBohm/bootstrap_arch/master/install_packages.sh \
+  && bash install_packages.sh
+
+
+exit
