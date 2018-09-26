0f99c55770eb157653dff9fc478e74db4ff97fd0
AxelBohm <axel.boehm@ymail.com>
2018-09-26 15:06:34 +0200

    connect and curl script should be placed on an arch iso

---
+++ connect_and_curl.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# connects to the internet (if wpa_supplicant config file is available)
+# and curls my bootstrapscript
+#
+# *use case*: this script can be put on iso (for example with a wpa_supplicant config file) which allows
+# for very easily installing of my personal arch setup.
+
+######################
+# connect to internet
+######################
+
+# name of wifi interface
+WIFI_INTERFACE="$(ip link | grep -o 'wlp.*:')"
+
+# remove colon from above string (there is no lookahead in grep so there is no
+# way to get the correct name from the regex right away)
+WIFI_INTERFACE=${WIFI_INTERFACE%?}
+
+# connect to internet
+read -r -p "Are you at school and do you want to connect to eduroam? [Y/n] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$|^$ ]]
+then
+    wpa_supplicant -B -i $WIFI_INTERFACE -c /etc/wpa_supplicant/wpa_supplicant.conf && dhcpcd $WIFI_INTERFACE
+else
+    echo "connect to the internet!" && exit
+fi
+
+
+#############################
+# curl actual install script
+#############################
+bash <(curl \
+    https://raw.githubusercontent.com/AxelBohm/bootstrap_arch/master/install.sh)
